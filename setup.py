33df09f6625b1ebe9ad550cb044850c9704dd2d3
Yukino Ikegami <yknikgm@gmail.com>
2016-04-14 17:28:18 +0900

    v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='pytypo',
     packages=find_packages(exclude=['test']),
-    version='0.1.0',
+    version='0.1.1',
     license='MIT License',
     platforms=['POSIX', 'Windows', 'Unix', 'MacOS'],
     description=('corrects English spelling mistakes and normalize.'
